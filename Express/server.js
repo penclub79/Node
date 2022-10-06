71d98dd462d06a41b5e4b0ae6f3d54b0310a2922
ShinHoCheol <penclub79@naver.com>
2022-10-06 11:34:56 +0900

    node express example

--- Express/server.js
+++ Express/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
-    return res.send('hello world!');
+const users = [];
+
+// 미들웨어 추가를 해야 req.body 객체를 parser해서 사용할 수 있다.
+app.use(express.json());
+
+app.get('/user', function(req, res){
+    res.send({users:users});
+})
+
+app.post('/user', function(req, res){
+    users.push({name : req.body.name, age : req.body.age});
+    return res.send({success : true});
 })
 
 app.listen(3000, function(){
     console.log('listening port 3000!')
-});
+})
